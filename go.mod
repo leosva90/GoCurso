967a20543bcefd5778db0de452fe3687ff8e636d
leosva90 <leosva90@gmail.com>
2024-04-27 23:06:22 -0300

    First Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/leosva90/GoCurso
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/leosva90/GoCurso/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
