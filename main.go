25dc4a9026522fa59e21bcf4e0999f193ac0b9fb
leosva90 <leosva90@gmail.com>
2024-05-12 19:37:45 -0300

    siguiente commit

--- main.go
+++ main.go
@@ -3,11 +3,31 @@ package main
 import (
 	"fmt"
 
-	"github.com/leosva90/GoCurso/variables"
+	"github.com/leosva90/GoCurso/ejercicios"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(1400)
+	/*estado, texto := variables.ConviertoaTexto(1400)
 	fmt.Println(estado)
+	fmt.Println(texto)
+	if os := runtime.GOOS; os == "linux" || os == "OS X." {
+		fmt.Println("Esto no es windows,es", os)
+
+	} else {
+		fmt.Println("Esto es windows")
+
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Println("%s \n", os)
+	} */
+
+	numero, texto := ejercicios.ConvNumerico("500")
+	fmt.Println(numero)
 	fmt.Println(texto)
 }
